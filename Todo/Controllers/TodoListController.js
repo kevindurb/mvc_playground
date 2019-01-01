6a968694c9e725bc53df67bed106e6647e49ec4d
Kevin Durbin <kevindurb@gmail.com>
2019-01-01 13:15:33 -0700

    render collections of todos

--- Todo/Controllers/TodoListController.js
+++ Todo/Controllers/TodoListController.js
@@ -1,9 +1,25 @@
 import { Controller } from '/lib/Controller.js';
 import { on, debug } from '/lib/helpers.js';
+import { TodoItemView } from '/Todo/Views/TodoItemView.js';
 
 const log = debug('TodoListController');
 
 export class TodoListController extends Controller {
   initialize() {
+    this.renderItems();
+
+    on(this.collection, 'change', this.renderItems);
+  }
+
+  renderItem(item) {
+    const view = new TodoItemView(this.view);
+    view.label.textContent = item.name;
+
+    this.view.appendView(view);
+  }
+
+  renderItems() {
+    this.view.clear();
+    this.collection.forEach(this.renderItem);
   }
 }
--- Todo/Todo.js
+++ Todo/Todo.js
@@ -1,15 +1,29 @@
 import { AddTodoInputController } from '/Todo/Controllers/AddTodoInputController.js';
 import { AddTodoInputView } from '/Todo/Views/AddTodoInputView.js';
+
+import { TodoListController } from '/Todo/Controllers/TodoListController.js';
+import { TodoListView } from '/Todo/Views/TodoListView.js';
+
 import { TodoCollection } from '/Todo/Models/TodoCollection.js';
 
 export class Todo {
   constructor() {
     this.$container = document.body;
     this.todoCollection = new TodoCollection();
-    this.addTodoInputView = new AddTodoInputView(this.$container);
+
+    this.addTodoInputView = new AddTodoInputView();
     this.addTodoInputController = new AddTodoInputController(
       this.addTodoInputView,
       this.todoCollection,
     );
+
+    this.todoListView = new TodoListView();
+    this.todoListController = new TodoListController(
+      this.todoListView,
+      this.todoCollection,
+    );
+
+    this.$container.appendChild(this.addTodoInputView.$el);
+    this.$container.appendChild(this.todoListView.$el);
   }
 }
